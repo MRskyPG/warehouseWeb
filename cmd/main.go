52bde4fd3c47297565b97d6b8c5627fd7d89765e
MRskyPG <mrogalsky8113@gmail.com>
2023-10-20 20:49:30 +0700

    Add functions to get access to DB and login

--- cmd/main.go
+++ cmd/main.go
@@ -2,27 +2,54 @@ package main
 
 import (
 	"context"
+	"fmt"
+	_ "github.com/lib/pq"
 	"github.com/sirupsen/logrus"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
+	"warehouseWeb/internal/sql"
 	"warehouseWeb/pkg/srv/httpserver"
 )
 
 func main() {
 	var srv httpserver.Server
 	fs := http.FileServer(http.Dir("./internal/frontend"))
-	//Using graceful shutdown
 	go func() {
 		if err := srv.Run("80", fs); err != nil {
 			log.Fatalf("Error occured while running http server: %s", err.Error())
 		}
 	}()
 
-	logrus.Print("Web-app Started")
+	db, err := sql.GetDB()
+	if err != nil {
+		panic("Didn't to Get Database.")
+	}
 
+	rows, err := db.Query("select login from workers")
+	if err != nil {
+		panic("Error. Database not found")
+	}
+	for rows.Next() {
+		var version string
+		_ = rows.Scan(&version)
+		fmt.Println(version)
+	}
+
+	access, err := sql.GetAccess(db, "vizzcon", "vizzcon")
+	if err != nil {
+		panic(err)
+	}
+	if access {
+		fmt.Println("true")
+	} else {
+		fmt.Println("false")
+	}
+
+	//Graceful Shutdown
+	logrus.Print("Web-app Started")
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
 	<-quit
@@ -31,4 +58,5 @@ func main() {
 	if err := srv.Shutdown(context.Background()); err != nil {
 		logrus.Errorf("Error was occured on server shutting down: %s", err.Error())
 	}
+
 }
--- internal/sql/sql.go
+++ internal/sql/sql.go
@@ -1,1 +1,42 @@
 package sql
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+func SetValuesForDB(port string, user string, password string, dbname string) string {
+	return fmt.Sprintf("user=%s password=%s dbname=%s port=%s sslmode=disable", user, password, dbname, port)
+}
+
+func GetDB() (*sql.DB, error) {
+	var err error
+	var db *sql.DB
+
+	//connStr := SetValuesForDB("postgres", "postgres", "postgres", "5432")
+	db, err = sql.Open("postgres", "user=postgres password=postgres dbname=postgres port=5432 sslmode=disable")
+	if err != nil {
+		return nil, err
+	}
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func GetAccess(db *sql.DB, login string, password string) (bool, error) {
+	var condition bool
+	str := fmt.Sprintf("select * from CHECKLOGIN('%s', '%s')", login, password)
+	rows, err := db.Query(str)
+	if err != nil {
+		return false, err
+	}
+
+	for rows.Next() {
+		_ = rows.Scan(&condition)
+	}
+
+	return condition, nil
+}
